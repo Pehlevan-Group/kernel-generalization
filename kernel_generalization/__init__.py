8b62d412ca4646d58925f78a18a42c0f6dd29e6c
canatara <60362246+canatara@users.noreply.github.com>
2021-01-15 15:01:33 -0500

    Update __init__.py

--- kernel_generalization/__init__.py
+++ kernel_generalization/__init__.py
@@ -1,1 +1,4 @@
-from kernel_generalization import kernel_generalization
+from kernel_generalization import gegenbauer
+from kernel_generalization import kernel_simulation
+from kernel_generalization import kernel_spectrum
+from kernel_generalization import kernel_regression
